c32fddc1cf572d2c2a44e53120e017a7bee6e1cf
slwu89 <slwu89@berkeley.edu>
2020-11-24 16:23:45 -0800

    sc hierarchical model

--- scripts/gelman_carpenter.R
+++ scripts/gelman_carpenter.R
@@ -5,10 +5,16 @@
 # --------------------------------------------------------------------------------
 
 rm(list=ls());gc()
+dev.off()
 library(rstan)
 library(nimble)
 library(ggplot2)
 rstan_options(auto_write = FALSE)
+
+
+# --------------------------------------------------------------------------------
+#   simplest model, no poststratification
+# --------------------------------------------------------------------------------
 
 santa_clara <- '
   data {
@@ -46,7 +52,7 @@ sc_model <- rstan::stan(
   warmup = 1e4,
   iter = 2e4,
   cores = 4,
-  verbose = TRUE
+  verbose = F
 )
 
 stan_trace(sc_model)
@@ -91,3 +97,80 @@ sc_model_cpp_mcmc_n <- nimble::compileNimble(sc_model_mcmc_n, project = sc_model
 samples <- nimble::runMCMC(mcmc = sc_model_cpp_mcmc_n,niter = 1e5,nburnin = 1e4,thin = 10,progressBar = TRUE)
 
 hist(samples[,"p"])
+
+
+# --------------------------------------------------------------------------------
+#   better model: hierarchical
+# --------------------------------------------------------------------------------
+
+
+sc_hier <- '
+  data {
+    int<lower = 0> y_sample;
+    int<lower = 0> n_sample;
+    int<lower = 0> J_spec;
+    int<lower = 0> y_spec[J_spec];
+    int<lower = 0> n_spec[J_spec];
+    int<lower = 0> J_sens;
+    int<lower = 0> y_sens[J_sens];
+    int<lower = 0> n_sens[J_sens];
+    real<lower = 0> logit_spec_prior_scale;
+    real<lower = 0> logit_sens_prior_scale;
+  }
+  parameters {
+    real<lower = 0, upper = 1> p;
+    real mu_logit_spec;
+    real mu_logit_sens;
+    real<lower = 0> sigma_logit_spec;
+    real<lower = 0> sigma_logit_sens;
+    vector<offset = mu_logit_spec, multiplier = sigma_logit_spec>[J_spec] logit_spec; // logit(gamma_j)
+    vector<offset = mu_logit_sens, multiplier = sigma_logit_sens>[J_sens] logit_sens; // logit(delta_j)
+  }
+  transformed parameters {
+    vector[J_spec] spec = inv_logit(logit_spec);
+    vector[J_sens] sens = inv_logit(logit_sens);
+  }
+  model {
+    real p_sample = p * sens[1] + (1 - p) * (1 - spec[1]); // SC frequency of positive tests (prev p corrected for imperfect tests)
+    y_sample ~ binomial(n_sample, p_sample); // SC data likelihood
+    y_spec ~ binomial(n_spec, spec); // likelihood for 13 spec studies
+    y_sens ~ binomial(n_sens, sens); // likelihood for 3 sens studies
+    logit_spec ~ normal(mu_logit_spec, sigma_logit_spec); // spec prior is normal on log-odds scale
+    logit_sens ~ normal(mu_logit_sens, sigma_logit_sens); // sens prior is normal on log-odds scale
+    sigma_logit_spec ~ normal(0, logit_spec_prior_scale); // hyperprior
+    sigma_logit_sens ~ normal(0, logit_sens_prior_scale); // hyperprior
+    mu_logit_spec ~ normal(4, 2);  // weak hyperprior on mean of distribution of spec
+    mu_logit_sens ~ normal(4, 2);  // weak hyperprior on mean of distribution of sens
+  }
+'
+
+sc_hier_data <- list(
+  # SC data
+  y_sample=50,
+  n_sample=3330,
+  # data from 13 specificity studies in Bendavid 2020b
+  J_spec=14,
+  y_spec=c(0, 368, 30, 70, 1102, 300, 311, 500, 198, 99, 29, 146, 105, 50),
+  n_spec=c(0, 371, 30, 70, 1102, 300, 311, 500, 200, 99, 31, 150, 108, 52),
+  # data from 3 sensitivity studies in Bendavid 2020b
+  J_sens=4,
+  y_sens=c(0, 78, 27, 25),
+  n_sens=c(0, 85, 37, 35)
+)
+
+# Fit with weak priors
+sc_hier_data$logit_spec_prior_scale <- 1
+sc_hier_data$logit_sens_prior_scale <- 1
+
+sc_hier_model <- rstan::stan(
+  model_code = sc_hier,
+  data = sc_hier_data,
+  chains = 4,
+  warmup = 1e4,
+  iter = 2e4,
+  cores = 4,
+  control=list(adapt_delta=0.95),
+  verbose = TRUE
+)
+
+stan_dens(sc_hier_model)
